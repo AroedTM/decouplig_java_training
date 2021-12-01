7c551f78657ce01d6b129a4b72feddd29af67afa
Enguerran Huby <ehuby@et.esiea.fr>
2021-12-01 22:46:06 +0100

    Partie 7

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,13 +1,18 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
 
     public static Logger getLogger(String name)
     {
         return new CompositeLogger(
-            new ContextualLogger(
-                new FileLogger("C://Users/engue/OneDrive/4A/S7/Architecture logiciel/TP Interfaces/logs.txt"),
-                name
+            new FilteredLogger(
+                new ContextualLogger(
+                    new FileLogger("C://Users/engue/OneDrive/4A/S7/Architecture logiciel/TP Interfaces/logs.txt"),
+                    name
+                ),
+                message -> name.contains("simulation")
             ),
             new ContextualLogger(
                 new ConsoleLogger(),
